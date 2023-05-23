78c78a35503ca685569101362e804973d96865b7
greatraksin <greatraksin@icloud.com>
2023-05-23 18:05:01 +0300

    Initial commit 🔥

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+import pygame as pg
+
+W = 1280
+H = 720
+FPS = 60
+clock = pg.time.Clock()
+
+pg.init()  # инициализируем pygame
+screen = pg.display.set_mode((W, H))  # создаем экран игры разрешением 1280х720px
+pg.display.set_caption('Ping Pong | PyGame')
+
+while True:  # цикл игры
+    clock.tick(FPS)
+    for event in pg.event.get():  # обработчик событий pygame
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
